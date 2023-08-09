d313f644146aa67d980df889bbaf74ec6ec27c2c
Brian <brian_@DESKTOP-GJH2BCP>
2023-08-09 18:11:47 -0400

    new commit for push

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,1 +1,7 @@
 // Your code here
+window.onload( () => {
+const gameSquare = document.getElementById("game")
+gameSquare.addEventListener('click', e => {
+   let targetSquare = document.getElementById(e.target);
+});
+})
